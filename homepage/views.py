f8e367d89128b64f2529d000e80757e8b488ec67
ahmed007dr <ahmed007eg@gmail.com>
2024-10-20 20:07:58 +0300

    view and home html

--- homepage/views.py
+++ homepage/views.py
@@ -1,7 +1,29 @@
+
 from django.shortcuts import render
+from about.models import AboutUs  
+from blog.models import BlogPost   
+from contact.models import Location 
+from testimonial.models import Testimonial 
+from team.models import TeamMember
+from event.models import Event
+from services.models import Service
 
-# Create your views here.
+def homepage(request):
+    about_info = AboutUs.objects.first()
+    blog_posts = BlogPost.objects.all().order_by('-date_posted')[:3]  
+    location = Location.objects.first()  
+    testimonials = Testimonial.objects.all()  
+    team_members = TeamMember.objects.all()  
+    events = Event.objects.all().order_by('date', 'time')  
+    services = Service.objects.all()  
 
+    context = {
+        'about_info': about_info,
+        'blog_posts': blog_posts,
+        'location': location,
+        'testimonials': testimonials,  
+        'team_members': team_members,  
+        'events': events,  
+        'services': services,   }
 
-def home(request):
-    return render(request, 'homepage/home.html')
+    return render(request, 'homepage/homepage.html', context)
