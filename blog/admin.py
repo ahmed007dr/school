84f8ed165f0355f14004b2dfd5869cde8a6af26f
ahmed007dr <ahmed007eg@gmail.com>
2024-10-24 01:05:14 +0300

    change path media

--- blog/admin.py
+++ blog/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from .models import BlogPost
+from .models import BlogPost,AdditionalImage
 
 class BlogPostAdmin(admin.ModelAdmin):
     list_display = ('title', 'author', 'date_posted')
     search_fields = ('title', 'author')
 
 admin.site.register(BlogPost, BlogPostAdmin)
+admin.site.register(AdditionalImage)
